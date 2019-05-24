10d350e1e918291f14b408bbd9963fa6990b2d7e
YUME\alakarraj <akrishnaswamy@rhythmone.com>
2019-05-24 18:20:12 +0530

    SASS Update

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var server = express();
 var port = process.env.PORT || 3000;
 // middleware
 server.use(express.static(__dirname + "/public"));
-
 // routes
 server.use("*", function(req, res) {
   // serve file
